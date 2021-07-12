8ea581590a24ea24c9070a80db8ac51016ab95e0
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-07-12 17:53:04 -0400

    Added random enemy spawning
    WIP - note that globals() needs to be refactored this is just temporary

---
+++ src/resources/enemy.py
@@ -0,0 +1,28 @@
+from random import randint
+
+from .AbstractDungeonEntity import AbstractDungeonEntity
+from .level import Level
+
+
+class Enemy(AbstractDungeonEntity):
+    """Enemy entity - hostile to players"""
+
+    def __init__(self, current_level: Level, symbol: str = '^') -> None:
+        super().__init__(
+            ground_symbol=current_level.board[
+                (x := randint(2, current_level.width - 2))
+            ][(y := randint(2, current_level.height - 2))],
+            x=x,
+            y=y,
+            symbol=symbol
+        )
+        self.current_level = current_level
+
+    def draw(self) -> None:
+        """Draws enemies to board"""
+        # while loop to ensure that enemies don't get placed in the same area
+        while str(self.current_level.board[self.y][self.x]) in (self.symbol, '$'):
+            self.x = randint(2, self.current_level.width - 2)
+            self.y = randint(2, self.current_level.width - 2)
+
+        self.current_level.board[self.y][self.x] = self.symbol
