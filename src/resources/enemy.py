66f8858088d1cc13b67f0e26306c92a64b3114fc
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-07-12 19:04:29 -0400

    Removed random spawning as requested

--- src/resources/enemy.py
+++ src/resources/enemy.py
@@ -1,5 +1,3 @@
-from random import randint
-
 from .AbstractDungeonEntity import AbstractDungeonEntity
 from .level import Level
 
@@ -9,20 +7,13 @@ class Enemy(AbstractDungeonEntity):
 
     def __init__(self, current_level: Level, symbol: str = '^') -> None:
         super().__init__(
-            ground_symbol=current_level.board[
-                (x := randint(2, current_level.width - 2))
-            ][(y := randint(2, current_level.height - 2))],
-            x=x,
-            y=y,
+            ground_symbol=current_level.board[5][5],
+            x=5,
+            y=5,
             symbol=symbol
         )
         self.current_level = current_level
 
     def draw(self) -> None:
         """Draws enemies to board"""
-        # while loop to ensure that enemies don't get placed in the same area
-        while str(self.current_level.board[self.y][self.x]) in (self.symbol, '$'):
-            self.x = randint(2, self.current_level.width - 2)
-            self.y = randint(2, self.current_level.width - 2)
-
         self.current_level.board[self.y][self.x] = self.symbol
