20f52d5112e65593a6a1e5abb1d7ab959f75142e
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-07-13 01:25:09 -0400

    Fixed keyboard input
    Using ``pynput`` instead of ``blessing`` and the ``threading module``

--- src/resources/GameResources.py
+++ src/resources/GameResources.py
@@ -10,7 +10,6 @@ class GameResources:
     def __init__(self):
         self.level = Level(10, 10, [1, 2, 3, 4], [])
         self.player = Character(self.level, "$")
-        self.player.start()
         self.test_enemy = Enemy(self.level)
         self.test_color_changer = ColorChanger(level=self.level, x=2, y=2, symbol="@", color="orange")
 
