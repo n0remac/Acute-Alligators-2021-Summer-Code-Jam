8757d39549d565aca3ff62a64c5c34fe3559a0e1
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-07-13 18:25:38 -0400

    Replaced mutating list to choose other direction to a ternary statement

--- src/resources/entities/Enemy.py
+++ src/resources/entities/Enemy.py
@@ -17,18 +17,16 @@ class Enemy(AbstractDungeonEntity):
     def mill(self) -> None:
         """Enemy randomly moves around"""
         movement_axis = choice(['x', 'y'])
-        direction_axis = [1, -1]
+        direction_axis = (1, -1)
         movement = choice(direction_axis)  # up or down (1, -1), left or right (1, -1)
         if movement_axis == 'x':  # movement will be on the x axis
             if (self.x <= 1 and movement == -1) or (self.x >= self.level.width - 2 and movement == 1):
-                direction_axis.remove(movement)
-                self.x += int(direction_axis[0])
+                self.x += 1 if movement == -1 else -1
             else:
                 self.x += movement
         if movement_axis == 'y':  # movement will be on the y axis
             if (self.y <= 1 and movement == -1) or (self.y >= self.level.height - 2 and movement == 1):
-                direction_axis.remove(movement)
-                self.y += int(direction_axis[0])
+                self.y += 1 if movement == -1 else -1
             else:
                 self.y += movement
 
