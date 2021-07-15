305e3dbcbfbe5dea3fd1f4246f06d1a18bc8bb91
Antsthebul <anthony.allen.srt@gmail.com>
2021-07-14 23:26:44 -0400

    Player color now changes.

--- src/resources/entities/ColorChanger.py
+++ src/resources/entities/ColorChanger.py
@@ -1,3 +1,5 @@
+import random
+
 from .AbstractDungeonEntity import AbstractDungeonEntity
 from .character import Character
 
@@ -5,18 +7,17 @@ from .character import Character
 class ColorChanger(AbstractDungeonEntity):
     """Dungeon Items that change players color if captured"""
 
+    _choices = ['blue', 'red', 'yellow']
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.color = "bold " + random.choice(self._choices)
+        self.symbol.stylize(self.color)
 
     def collisions_with_player(self, x: int, y: int) -> bool:
         """Checks if player collided with enemy"""
         return ((self.x, self.y) == (x, y-1)) or ((self.x, self.y) == (x-1, y))
 
-    def _reset(self) -> None:
-        """Resets symbol to normal game 'tick'"""
-        self.symbol = self.ground_symbol
-
     def change_color(self, player: Character) -> None:
         """Will change color of player instance and reset to normal game tile"""
-        player.symbol.stylize(self.style)
-        self._reset()
+        player.symbol.stylize(self.color)
