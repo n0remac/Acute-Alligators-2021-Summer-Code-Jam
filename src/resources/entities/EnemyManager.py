d0b076eb05c7d321a80c5d8df140f80a58094cf1
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-07-15 16:51:49 -0400

    Added a ``file`` attr to enemies for information panel.

--- src/resources/entities/EnemyManager.py
+++ src/resources/entities/EnemyManager.py
@@ -12,16 +12,17 @@ class EnemyManager:
         self.enemy_list = []
         self.level = level
 
-    def spawn_random_enemies(self, x_player: int, y_player: int, num: int) -> None:
+    def spawn_random_enemies(self, x_player: int, y_player: int, files_in_dir: list) -> None:
         """Spawns a new enemies randomly"""
-        while num > 0:
+        num = len(files_in_dir) - 1
+        while num + 1 > 0:
             y = randint(2, self.level.height-2)
             x = randint(2, self.level.width-2)
             disallowed_spaces = {'x': (x_player - 1, x_player + 1), 'y': (y_player - 1, y_player + 1)}
             if str(self.level.board[y][x]) == "'" and \
                     x not in disallowed_spaces['x'] and y not in disallowed_spaces['y']:
                 num -= 1
-                enemy = Enemy(aggro_radius=3, x=x, y=y, symbol='^')
+                enemy = Enemy(aggro_radius=3, x=x, y=y, symbol='^', file=files_in_dir[num])
                 self.enemy_list.append(enemy)
 
     def collisions_with_player(self, x: int, y: int) -> bool:
