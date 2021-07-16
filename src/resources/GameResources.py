b27a96ade5329609dca2c9b68ff9650e14632101
n0remac <smartcameron@gmail.com>
2021-07-15 21:57:19 -0700

    fix bugs

--- src/resources/GameResources.py
+++ src/resources/GameResources.py
@@ -32,12 +32,16 @@ class GameResources:
                     (entity.__class__.__name__ != 'Enemy' and str(
                         self.level.board[entity.y + y][entity.x + x]) == '#'):
                 self.level.board[entity.y][entity.x] = entity.ground_symbol
-                if entity.x + x > 0 and entity.y + y > 0:
+                if entity.entity_type == "enemy":
+                    if entity.x + x > 0 and entity.y + y > 0:
+                        entity.x += x
+                        entity.y += y
+                    else:
+                        entity.x -= x
+                        entity.y -= y
+                else:
                     entity.x += x
                     entity.y += y
-                else:
-                    entity.x -= x
-                    entity.y -= y
                 entity.ground_symbol = self.level.board[entity.y][entity.x]
                 entity.new_positions = {"x": 0, "y": 0}
         except IndexError:
