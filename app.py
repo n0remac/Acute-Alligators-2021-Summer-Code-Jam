a7c68d473a35c1106b94218db09ad62362fbd57b
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-07-15 22:13:03 -0400

    Tree panel & level treversal is complete

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from rich.live import Live
 from rich.panel import Panel
 from rich.text import Text
 
-from src.resources.GameResources import GameResources
+from src.GameResources import GameResources
 from src.resources.PanelLayout import PanelLayout
 
 
@@ -32,7 +32,9 @@ def run_game(layout: Layout, game_resources: GameResources) -> Panel:
     # Panels to update
     layout["main_game"].update(panel)
     layout["footer"].update(Panel('footer'))
-    layout["tree"].update(Panel('tree'))
+    layout["tree"].update(
+        Panel(game_resources.node.display_node(), title="Current Location")
+    )
     sleep(0.1)
 
 
@@ -47,7 +49,9 @@ def main() -> None:
 
     # Panels to update
     layout["footer"].update(Panel('footer'))
-    layout["tree"].update(Panel('tree'))
+    layout["tree"].update(
+        Panel(game_resources.node.display_node(), title="Current Location")
+    )
 
     with Live(layout, refresh_per_second=10, screen=True):
         while game_resources.player.playing:
--- src/LevelSelector.py
+++ src/LevelSelector.py
@@ -1,3 +1,5 @@
+from random import choice
+
 from .fstree.FileStructureTree import FileStructureTree
 from .resources.entities.level.Level import Level
 
@@ -5,11 +7,13 @@ from .resources.entities.level.Level import Level
 class LevelSelector:
     """Creates and stores levels"""
 
-    def __init__(self):
-        self.file_structure = FileStructureTree(".")
+    def __init__(self, fsf: FileStructureTree):
+        self.file_structure = fsf
         self.cur = self.file_structure.root
 
-    def create_level(self, door: (int, int) = (0, 0)) -> None:
+    def create_level(self, door: (int, int) = (0, 0), *, game_started: bool) -> None:
         """Creates a Level"""
+        if game_started:
+            self.cur = choice(self.cur.children)
         level = Level(10, 15, self.cur.children, door)
         return level
