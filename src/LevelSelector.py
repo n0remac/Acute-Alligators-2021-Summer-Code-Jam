bf4cfe6b7834ebf1ff6452a21f940edaabf7d523
n0remac <smartcameron@gmail.com>
2021-07-15 20:02:17 -0700

    Started level designer

--- src/LevelSelector.py
+++ src/LevelSelector.py
@@ -9,7 +9,12 @@ class LevelSelector:
         self.file_structure = FileStructureTree(".")
         self.cur = self.file_structure.root
 
-    def create_level(self, door: (int, int) = (0, 0)) -> None:
+    def create_level(self, door: (int, int) = (0, 0)) -> Level:
         """Creates a Level"""
-        level = Level(10, 15, self.cur.children, door)
+
+        level = Level(10, 15, self.cur)
+        level.generate_level()
+        level.create_doors(door)
+        level.cur_node = self.cur
+
         return level
